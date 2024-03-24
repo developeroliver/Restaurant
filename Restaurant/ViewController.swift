9c7be28262c2fab7296ac2f10dd7557ef7fb21f7
olivier geiger <developpeur.olive@gmail.com>
2024-03-24 10:59:40 +0100

    init project

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by olivier geiger on 24/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
